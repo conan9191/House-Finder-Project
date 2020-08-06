c2e43cee86631ab96787b747cdcf2bbe06c8f2a7
conan9191 <pengyiqun@163.com>
2020-08-05 21:50:58 -0400

    update the directory structure

--- data/users.js
+++ data/users.js
@@ -4,7 +4,7 @@
  * @Author: Yiqun Peng
  * @Date: 2020-08-05 14:20:09
  * @LastEditors: Yiqun Peng
- * @LastEditTime: 2020-08-05 15:48:02
+ * @LastEditTime: 2020-08-05 21:39:04
  */
 const mongoCollections = require('../config/mongoCollections');
 const bands = mongoCollections.users;
@@ -20,7 +20,18 @@ let exportedMethods = {
         const userList = await userCollection.find().toArray();
         if (!userList|| userList.length === 0) throw 'No users in database!';
         return userList;
+    },
+
+    async getUserById(id) {
+        if(!id) throw "You must provide an id";
+        if (typeof id !== "string") throw "ID needs to be a string";
+        const objId = ObjectId.createFromHexString(id);
+        const userCollection = await recipes();
+        const user = await userCollection.findOne({ _id: objId });
+        if (!user) throw 'Recipe not found';
+        return user;
     }
+
 };
 
 module.exports = exportedMethods;
