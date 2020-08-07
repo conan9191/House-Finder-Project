7aee8edc8ec7049f9172e0c823da51df42160be7
conan9191 <pengyiqun@163.com>
2020-08-07 18:48:48 -0400

    Complete the routing and base pages of the personal center

---
+++ routes/account.js
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2020-08-07 16:20:41
+ * @LastEditors: Yiqun Peng
+ * @LastEditTime: 2020-08-07 16:23:07
+ */
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const userData = data.users;
+
+router.get('/:id', async (req, res) => {
+
+    let user = await userData.getUserById(req.params.id);
+    res.render('pages/account', { title: "User Account", user: user });
+  });
+
+
+  module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@
  * @Author: Yiqun Peng
  * @Date: 2020-08-05 21:43:34
  * @LastEditors: Yiqun Peng
- * @LastEditTime: 2020-08-06 01:17:32
+ * @LastEditTime: 2020-08-07 16:37:24
  */
 const userRoutes = require('./users');
+const accountRoutes = require('./account');
 
 const constructorMethod = (app) => {
   app.use('/users', userRoutes);
+  app.use('/account', accountRoutes);
   app.use('*', (req, res) => {
     res.sendStatus(404);
   });
