c92f0bebb1933aadf5a13a72695e605674f6c068
conan9191 <pengyiqun@163.com>
2020-08-14 06:25:40 -0400

    Fixed a bug in Review where the Delete button did not react when clicked

--- public/js/review.js
+++ public/js/review.js
@@ -2,26 +2,24 @@
  * @Descripttion: 
  * @version: 
  * @Author: sueRimn
- * @Date: 2020-08-13 09:34:05
+ * @Date: 2020-08-14 06:21:43
  * @LastEditors: Yiqun Peng
- * @LastEditTime: 2020-08-13 10:33:47
+ * @LastEditTime: 2020-08-14 06:23:22
  */
-$(document).ready(function(){
-    let id = $('#deleteBtn').val();
+function deletereview(){
+    let buttonId = event.srcElement.value;
+    let id = buttonId;
     let houseId = $('input[name="houseId"]').val();
     let requestConfig = {
-        method: 'DELETE',
-        url: '/review/' + id,
-        contentType: 'application/json',
-        data: JSON.stringify({
-            id: id,
-            houseId: houseId
-        })
+    method: 'DELETE',
+    url: '/review/' + id,
+    contentType: 'application/json',
+    data: JSON.stringify({
+        id: id,
+        houseId: houseId
+    })
     }
-    $('#deleteBtn').click(function () {
-        $.ajax(requestConfig).then(function (responseMessage) {
-            window.location.replace("http://localhost:3000/house/"+houseId);
-        });
+    $.ajax(requestConfig).then(function (responseMessage) {
+        window.location.replace("http://localhost:3000/house/"+houseId);
     });
-
-});
+}
