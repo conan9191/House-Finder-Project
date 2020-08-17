2e45c0bc2b2e2df049452584b4d2a4a1d4233388
syedmahvish <syedmahvish822@gmail.com>
2020-08-17 14:52:14 -0400

    User profile picture display issue : account,homepage,review,comment - done

--- public/js/profilePicture.js
+++ public/js/profilePicture.js
@@ -1,42 +1,54 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: sueRimn
  * @Date: 2020-08-16 15:22:12
  * @LastEditors: Yiqun Peng
  * @LastEditTime: 2020-08-16 23:25:58
  */
 
-function imageOnClick(){
-    document.getElementById('updatepicture').click();
+function imageOnClick() {
+  document.getElementById("updatepicture").click();
 }
 
 function updateimage() {
-    let img = document.getElementById('updatepicture').files[0].name;
-    let requestConfig = {
-    method: 'PATCH',
-    url: '/account',
-    contentType: 'application/json',
+  let file = document.getElementById("updatepicture").files[0];
+  const FR = new FileReader();
+  FR.addEventListener("load", (evt) => {
+    const img = new Image();
+    // img.height = "100";
+    // img.width = "100";
+    img.src = evt.target.result;
+    updateImageAPIcall(img.src);
+    // profilePictureArray.push(img.src);
+  });
+  FR.readAsDataURL(file);
+}
+
+function updateImageAPIcall(imageSource) {
+  let requestConfig = {
+    method: "PATCH",
+    url: "/account",
+    contentType: "application/json",
     data: JSON.stringify({
-      profilePicture: img,
-    })
-    }
-    $.ajax(requestConfig).then(function (responseMessage) {
-        console.log(responseMessage);
-        reloadAccount();
-    });
+      profilePicture: imageSource,
+    }),
+  };
+  $.ajax(requestConfig).then(function (responseMessage) {
+    console.log(responseMessage);
+    reloadAccount();
+  });
 }
 
 function reloadAccount() {
-   
-    let xhttpget = new XMLHttpRequest();
-    method = "GET";
-    url = "/account";
-  
-    xhttpget.open(method, url, true);
-    xhttpget.setRequestHeader("Content-type", "application/json");
-    xhttpget.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-    xhttpget.send();
-  
-    location.reload();
-  }
+  let xhttpget = new XMLHttpRequest();
+  method = "GET";
+  url = "/account";
+
+  xhttpget.open(method, url, true);
+  xhttpget.setRequestHeader("Content-type", "application/json");
+  xhttpget.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  xhttpget.send();
+
+  location.reload();
+}
